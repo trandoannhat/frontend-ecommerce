827b6ae26dc3ac58e5c9b2dd5ee3c4d7d2af4a8d
Nhất Trần Doãn <trandoannhat@yahoo.com>
2025-11-22 22:54:12 +0700

    update2

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local = "http://localhost:5000";
-const production = "https://backend-api-ecommerce-ecv5.onrender.com";
+const production = "https://api-ecommerce-kxj4.onrender.com";
 
 let api_url = "";
 let mode = "pro";
